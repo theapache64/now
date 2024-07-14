bb8a89d49714f62be9a8b3cc132e834f708c7427
theapache64 <theapache64@gmail.com>
2024-07-15 03:12:23 +0530

    📈 improve fakeUrlModule

--- app/src/benchmark/java/com/github/theapache64/now/di/FakeUrlModule.kt
+++ app/src/benchmark/java/com/github/theapache64/now/di/FakeUrlModule.kt
@@ -10,16 +10,20 @@ import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
+import java.util.Date
 import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class FakeUrlModule {
 
     @Provides
+    @Singleton
     @Named("world_time_api_url")
     fun provideWorldTimeApiUrl(): String {
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+
         val server = MockWebServer()
         server.dispatcher = object : Dispatcher() {
 
@@ -30,7 +34,7 @@ class FakeUrlModule {
                             .setResponseCode(200)
                             .setBody(
                                 """
-                              {"datetime":"1997-01-07T00:00:00.000000+05:30"}
+                              {"datetime":"${Date()}"}
                           """.trimIndent()
                             )
                     }
